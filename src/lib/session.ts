55d9e308d5c47fc3553ee293b0aee340d6c22013
Ifeanyi Anyanwu <anyanwuifeanyi001@gmail.com>
2025-08-01 14:03:09 +0100

    feat: add session management functions for token decryption and deletion

---
+++ src/lib/session.ts
@@ -0,0 +1,23 @@
+import "server-only";
+import { jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export async function decrypt(session: string | undefined = "") {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+}
+
+export async function deleteSession() {
+  const cookieStore = await cookies();
+  cookieStore.delete("access_token");
+  cookieStore.delete("refresh_token");
+}
